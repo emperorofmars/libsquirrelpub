3e6f7e3df08d75559230223e3e3034f64874f4bc
emperorofmars <mars@squirrel.pub>
2024-11-28 02:23:44 +0100

    update testing

--- test/payload_test.ts
+++ test/payload_test.ts
@@ -17,11 +17,36 @@ Deno.test({
 
 		assertSpyCalls(spy, 2);
 		assertSpyCallArgs(spy, 0, [test_object.fetch_url]);
+		assertSpyCallArgs(spy, 1, [test_object.fetch_url + "/verify.txt"]);
 
 		assertEquals(payload.payload, JSON.stringify(test_object.json));
 		assertEquals(payload.original_url, test_object.fetch_url);
 		assertEquals(payload.signature, "jPXN6s3YE1Faki7aCquo/KzMHx3wQ50KiEzLwRKstN2BQCWXuZPCTKsujj2XOYfXQcQp1Buw1gmdIiNbbJIXDQ==");
 		
 		assertEquals(await payload.verify(await importKey(testObjects.test_identity_public_key)), false);
+		assertEquals(await payload.verify(await importKey(testObjects.test_identity_public_key), "jPXN6s3YE1Faki7aCquo/KzMHx3wQ50KiEzLwRKstN2BQCWXuZPCTKsujj2XOYfXQcQp1Buw1gmdIiNbbJIXDQ=="), false);
+	}
+});
+
+Deno.test({
+	name: "Fetch Payload custom signature url",
+	async fn() {
+		const test_object = testObjects.identity_valid_minimal;
+
+		const spy = fetchSequenceSpy([JSON.stringify(test_object.json), "jPXN6s3YE1Faki7aCquo/KzMHx3wQ50KiEzLwRKstN2BQCWXuZPCTKsujj2XOYfXQcQp1Buw1gmdIiNbbJIXDQ=="]);
+		const payload = await SquirrelpubPayload.fetch(test_object.fetch_url, "https://foo.com/asdf");
+
+		console.log(payload);
+
+		assertSpyCalls(spy, 2);
+		assertSpyCallArgs(spy, 0, [test_object.fetch_url]);
+		assertSpyCallArgs(spy, 1, ["https://foo.com/asdf"]);
+
+		assertEquals(payload.payload, JSON.stringify(test_object.json));
+		assertEquals(payload.original_url, test_object.fetch_url);
+		assertEquals(payload.signature, "jPXN6s3YE1Faki7aCquo/KzMHx3wQ50KiEzLwRKstN2BQCWXuZPCTKsujj2XOYfXQcQp1Buw1gmdIiNbbJIXDQ==");
+		
+		assertEquals(await payload.verify(await importKey(testObjects.test_identity_public_key)), false);
+		assertEquals(await payload.verify(await importKey(testObjects.test_identity_public_key), "jPXN6s3YE1Faki7aCquo/KzMHx3wQ50KiEzLwRKstN2BQCWXuZPCTKsujj2XOYfXQcQp1Buw1gmdIiNbbJIXDQ=="), false);
 	}
 });
